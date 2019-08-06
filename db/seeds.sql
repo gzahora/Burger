6664e487f2e5bba9aaccf0ed44db4da1983612ba
Greg Zahora <gregzahora3@gmail.com>
2019-08-06 17:00:16 -0400

    created initial file structure

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Gourdon-Hamsey Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Poutine on the Ritz Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Poblano Picasso Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Eggers Can't Be Cheesers Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Onion Ring Around the Rosemary Burger");
